4c4ef48260a97ffb4c35da17a69a46709bf6b765
Nathan Cheadle <natecheadle@gmail.com>
2024-06-13 16:55:12 -0700

    Added additional tests

--- test/dll_object_pool_tests.cpp
+++ test/dll_object_pool_tests.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 
 #include <dll_object_pool.hpp>
+#include <future>
 #include <iostream>
 #include <vector>
 
@@ -67,6 +68,79 @@ TEST_F(DllObjectPoolFixture, ValidateCreateDestroyTillFullMultiple) {
     for (auto obj : objs) {
       m_Pool.Destroy(obj);
     }
+  }
+}
+
+TEST_F(DllObjectPoolFixture, ValidateCreateDestroyReverseOrder) {
+  for (int i = 0; i < 5; ++i) {
+    std::vector<Data*> objs;
+
+    for (size_t i = 0; i < kPoolSize; ++i) {
+      objs.push_back(m_Pool.Create(Data(i, (double)i)));
+    }
+    for (auto obj : objs) {
+      ASSERT_NE(obj, nullptr);
+    }
+    ASSERT_EQ(m_Pool.Create(Data(11, 11.0)), nullptr);
+    for (auto it = objs.rbegin(); it != objs.rend(); ++it) {
+      m_Pool.Destroy(*it);
+    }
+  }
+}
+
+TEST_F(DllObjectPoolFixture, ValidateCreateDestroyOutOfOrder) {
+  for (int i = 0; i < 5; ++i) {
+    std::vector<Data*> objs;
+
+    for (size_t i = 0; i < kPoolSize; ++i) {
+      objs.push_back(m_Pool.Create(Data(i, (double)i)));
+    }
+    for (auto obj : objs) {
+      ASSERT_NE(obj, nullptr);
+    }
+    ASSERT_EQ(m_Pool.Create(Data(11, 11.0)), nullptr);
+    for (size_t i = 0; i < kPoolSize; ++i) {
+      if (i % 2 == 0) {
+        m_Pool.Destroy(objs[i]);
+        objs[i] = nullptr;
+      }
+    }
+
+    for (size_t i = 0; i < kPoolSize; ++i) {
+      if (objs[i]) m_Pool.Destroy(objs[i]);
+      objs[i] = nullptr;
+    }
+  }
+}
+
+TEST_F(DllObjectPoolFixture, ValidateCreateDestroyTillFullMultipleThreads) {
+  auto createDestroy = [&]() -> bool {
+    try {
+      std::vector<Data*> objs;
+
+      for (size_t i = 0; i < kPoolSize / 2; ++i) {
+        objs.push_back(m_Pool.Create(Data(i, (double)i)));
+      }
+      for (auto obj : objs) {
+        if (obj == nullptr) {
+          return false;
+        }
+      }
+      for (auto obj : objs) {
+        m_Pool.Destroy(obj);
+      }
+    } catch (...) {
+      return false;
+    }
+    return true;
+  };
+
+  for (int i = 0; i < 5; ++i) {
+    std::future thread1 = std::async(createDestroy);
+    std::future thread2 = std::async(createDestroy);
+
+    ASSERT_TRUE(thread1.get());
+    ASSERT_TRUE(thread2.get());
   }
 }
 }  // namespace ignosi::memory::test
