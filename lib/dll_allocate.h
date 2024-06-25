2bda91efdf4eb94b4f123e4370783604285f1238
Nathan Cheadle <92184613+natecheadle@users.noreply.github.com>
2024-06-24 21:50:48 -0700

    Reorganized repository and naming (#16)
    * Reorganized repository and naming

--- lib/dll_allocate.h
+++ lib/dll_allocate.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <dll_defines.h>
-
 #include <cstddef>
 
 extern "C" {
--- lib/dll_object_pool.h
+++ lib/dll_object_pool.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <dll_defines.h>
-
 #include <cstddef>
 
 using IgnosiMemoryPool = void*;
@@ -15,5 +13,4 @@ EXPORT_SPEC void* IgnosiMemoryPoolAllocate(IgnosiMemoryPool pool);
 EXPORT_SPEC void IgnosiMemoryPoolDeallocate(IgnosiMemoryPool pool, void* pObj);
 EXPORT_SPEC size_t IgnosiMemoryPoolSize(IgnosiMemoryPool pool);
 EXPORT_SPEC size_t IgnosiMemoryPoolAllocatedCount(IgnosiMemoryPool pool);
-
 }
